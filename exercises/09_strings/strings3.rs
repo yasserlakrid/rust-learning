d06e66c350e0617f5bb7693b63e00652388a887c
yasser lakrid <yasserlakrid36@gmail.com>
2026-08-07 20:14:45 +0100

    solve the first 34 exercises

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,25 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    let mut inpute = &mut input ; 
+    for i in 0..input.len() {
+        if inpute[i] == ' ' {
+            inpute.trim();
+        }
+    }; 
+     
+
+    *inpute 
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+ String::from(input)    
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    String::from(String::from(input)+ " world!")
+
 }
 
 fn main() {
