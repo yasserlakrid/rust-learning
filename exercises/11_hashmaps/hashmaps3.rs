2d6772b25198d193d6d0734422a0737c0b01d9ae
yasser lakrid <yasserlakrid36@gmail.com>
2026-08-12 19:59:27 +0100

    46

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -21,12 +21,29 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
 
     for line in results.lines() {
         let mut split_iterator = line.split(',');
+        
         // NOTE: We use `unwrap` because we didn't deal with error handling yet.
+      
+
         let team_1_name = split_iterator.next().unwrap();
         let team_2_name = split_iterator.next().unwrap();
         let team_1_score: u8 = split_iterator.next().unwrap().parse().unwrap();
         let team_2_score: u8 = split_iterator.next().unwrap().parse().unwrap();
-
+        
+        let team1 = scores.entry(team_1_name).or_insert(TeamScores {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        
+        team1.goals_scored +=  team_1_score ;
+        team1.goals_conceded += team_2_score ; 
+        let team2 = scores.entry(team_2_name).or_insert(TeamScores {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team2.goals_scored +=  team_2_score ;
+        team2.goals_conceded += team_1_score ; 
+       
         // TODO: Populate the scores table with the extracted details.
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
