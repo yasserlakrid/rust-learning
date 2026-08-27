209fd2707d16e4810ff50b5e40a4939209f966fa
yasser lakrid <yasserlakrid36@gmail.com>
2026-08-27 12:58:31 +0100

    adding delete , finaly

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 #[allow(dead_code)]
-mod my_stack ; 
-use my_stack::Mstack; 
+
 mod trees ; 
 use trees::Mtree;
 
 fn main() {
     
-    let mut tree = Mtree::new(); 
+    let mut tree = Mtree::new();
+     tree.insert(4);
     tree.insert(1);
-    tree.insert(4);
+    
     tree.insert(10);
     tree.insert(2);
-    tree.print(); 
 
+    tree.delete(2); 
+    tree.print(); 
+    
+    println!("{:?} {:?}"  , tree.search(4) , tree.search(2) );
 
     
 
--- src/trees.rs
+++ src/trees.rs
@@ -28,16 +28,61 @@ impl Mtree {
             }
         }
     }
-    
+
     pub fn print(&self){
         if let Some(root) = &self.root  {
             root.print()
         }
        
     }
+    pub fn search(&self , value : i32) -> bool {
+        match &self.root {
+            Some(root)=> {
+                root.search(value)
+            },
+            None => false 
+        }
+    }
+    pub fn delete(&mut self , target : i32)->bool{ 
+        if let Some(root) = &self.root {
+            
+            return delete(&mut self.root , target)
+        }
+        return false
+    }
+
 }
+fn delete(root : &mut Option<Box<Node>> , target : i32 )->bool{
+    return match root {
+        Some(node)=>{//node is Node
+            if node.value < target {
+                return delete(&mut node.rc , target)
+            }else if node.value > target {
+                return delete(&mut node.lc , target)
+            }
+            match (&node.rc , &node.lc) {
+                (None , None ) => {*root = None}
+                (Some(r) , None) => {*root = node.rc.take()}
+                (None , Some(l)) => {*root =  node.lc.take()}
+                (Some(rc) , Some(lc)) => {
+                    let mut succ = rc ; //succ is &box node 
+                    while let Some(next) = succ.lc.as_ref() { // next is &box node
+                        succ = next ;
+                    };
+                    node.value = succ.value ; 
+                    delete(&mut node.rc , node.value) ; 
+                    
+                }
+            }   
+            true
+        },
+        None=>false
+    }
+}
+
 impl Node {
-    pub fn print(&self){
+    
+    fn print(&self){
             if let Some(left_child) = &self.lc {
                 left_child.print();
 
@@ -50,8 +95,20 @@ impl Node {
 
               }
     }
-
-    pub fn insert(&mut self , value_in : i32){
+     fn my_clone(&self) -> Node {
+        Node {
+            value: self.value,
+            rc: match &self.rc {
+                Some(right) => Some(Box::new(right.my_clone())) ,
+                None => None
+            } ,  
+            lc: match &self.lc {
+                Some(left) => Some(Box::new(left.my_clone())) ,
+                None => None
+            } 
+        }
+    }
+     fn insert(&mut self , value_in : i32){
       
            
                 if self.value < value_in {
@@ -69,8 +126,32 @@ impl Node {
                         }))
                     }
                 }
-        
-
-           
         }
-    } 
+    fn search(&self , value : i32) -> bool {
+       
+            
+                if self.value == value {
+                    true
+                }else{
+                    if self.value > value {
+                        match &self.lc {
+                            Some(left_child)=>{
+                             left_child.search(value)
+                            },
+                            None => false
+                        }
+                    }else{
+                         match &self.rc {
+                            Some(right_child)=>{
+                             right_child.search(value)
+                            },
+                            None => false
+                        }
+                       
+                    }
+                }
+            
+            
+            }
+
+        }
