ba2aadc9045db9f6d054bac88712a6d4889c06c9
yasser lakrid <yasserlakrid36@gmail.com>
2026-09-03 15:36:02 +0100

    fixes

--- multi-thread-programs/src/main.rs
+++ multi-thread-programs/src/main.rs
@@ -1,26 +1,18 @@
 use std::net::{TcpListener , TcpStream};
+use std::sync::{Mutex , Arc}; 
 use std::io::{Read , Write , BufRead , BufReader }; 
 use std::thread ; 
 
 fn main() {
-    let mut threads = Vec::new(); 
-    let mut x = 0; 
-    for _ in 1..=5 {
-        let thread = thread::spawn(move || {
-            x += 1 ;
-            x 
-        }); 
-        threads.push(thread)
-    }
-    let mut new_x = 0 ; 
-    let mut i = 0 ; 
-    for thread in threads {
-            i+=1 ; 
-            if i == 5{
-             new_x = thread.join().unwrap(); 
-            }
-    }
-    
-    println!("if this value : {} isn't 1 than i'm stupid " , new_x); 
+
+    let data = Arc::new(Mutex::new(vec![1, 2, 3]));
+
+    let handle = std::thread::spawn(move || {
+        let data = Arc::clone(&data); 
+        
+        println!("{:?}", data.lock().unwrap());
+    });
+
+    handle.join().unwrap();
 
 }   
