84e44dbeb98cb3e40b992cb8b30411f3a55397db
yasser lakrid <yasserlakrid36@gmail.com>
2026-09-12 16:40:26 +0100

    playing async

--- asyncawait/asyncRust/src/main.rs
+++ asyncawait/asyncRust/src/main.rs
@@ -1,16 +1,28 @@
 use std::time::Duration;
+use trpl ;
+use trpl::Either;
+use std::sync::{Arc, Mutex};
+async fn  slow(duration : Duration) ->Duration{
+            trpl::sleep(duration).await;
+            duration
+        }
+        async fn  timeOut<F:Future>(fut : F, duration : Duration) -> Result<F::Output, Duration>{
+            match trpl::select(fut , trpl::sleep(duration)).await{
+                Either::Left(output) => Ok(output),
+                Either::Right(_) => Err(duration)
+            }
+        }
+
 fn main(){
     trpl::block_on(async {
-            trpl::spawn_task(async{
-                        for i in 1..5 {
-            println!("inner i = {}", i);
-            trpl::sleep(Duration::from_secs(1)).await;
+       
+        
+
+        let result = timeOut(slow(Duration::from_secs(4)), Duration::from_secs(2)).await;
+        match result {
+            Ok(output) => println!("output is {}", output.as_secs()),
+            Err(duration) => println!("timed out after {} seconds", duration.as_secs())
         }
-        });
-        for i in 1..5 {
-        println!("outer i = {}", i);
-        trpl::sleep(Duration::from_secs(1)).await;
-    }
     });
-    println!("main thread end");
+   
 }
